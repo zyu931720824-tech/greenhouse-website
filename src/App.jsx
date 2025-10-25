2024b6aa7faaa7b1e1c36e27f44db502a563d182
zyu931720824-tech <zyu931720824@gmail.com>
2025-10-25 17:12:10 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import  Narbar  from './components/Narbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import  Workflow  from './components/Workflow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Narbar />
+      <div className='max-w-7xl mx-auto pt-0 px-6'>
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
